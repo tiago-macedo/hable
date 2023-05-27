2e8977a0e89dbbaf348b4c6af4cc1fb27a67ff29
tiago-macedo <tiagomacedo@ufrj.br>
2023-05-26 23:39:03 -0300

    Implement Table class

---
+++ lib/hable/hable.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Hable
+  
+  class Table
+    attr_reader :header, :stub
+    attr_accessor :data
+
+    def initialize
+      @data = Hash.new
+    end
+
+    def data
+      @data
+    end
+
+    def data=(new_data)
+      @data = new_data
+    end
+
+    def head(*columns)
+      fail unless @data.empty? # TODO: add error
+
+      @header = columns
+      columns.each { |column_name| @data[column_name] = {} }
+    end
+      
+
+    def row(lable, *data)
+      data.each_with_index do |datum, idx|
+        column = header[idx]
+        @data[column][lable] = datum
+      end
+    end
+
+    def [](column, row)
+      @data.dig(column, row)
+    end
+  end
+end
